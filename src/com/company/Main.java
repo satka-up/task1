cd844fb5b8638122961de18a9d4bab0727d35dc2
Алексей <satka@list.ru>
2020-06-15 16:58:58 +0500

    3+4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         int a = 3;
-        System.out.println(a);
+        int b = 4;
+        System.out.println(a+b);
     }
 }
